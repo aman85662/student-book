395adc60f5dd16b7144aa1f9587241a92f38eb83
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-11-14 15:15:46 +0530

    changes in js file

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const students= ["aman", "amit", "sumit", "ravi"];
-const studentCulture = document.getElementById("record");
+
 
 function displayStudents() {
-    studentCulture.innerHTML = '';
+    const studentCulture = document.getElementById("record");
+    studentCulture.innerHTML = "";
     for (let i = 0; i < students.length; i++) {
         studentCulture.innerHTML += `<p>${i + 1}) ${students[i]}</p>`;
     }
 }
-document.getElementById("studentForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const studentName = document.getElementById("studentName").value;
-        students.push(studentName);
-        displayStudents();
-        document.getElementById("studentName").value = '';
-});
-
 displayStudents();
-
+function addStudents(){
+    const studentInput= document.getElementById("studentName");
+    students.push(studentInput.value);
+    studentInput.value = "";
+   displayStudents();
+  }
+displayStudents();
